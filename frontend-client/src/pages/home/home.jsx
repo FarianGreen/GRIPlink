3bd686607e15487077c55cb41e9a688329bf6012
FarianGreen <aremfarafonov6@gmail.com>
2024-01-12 19:46:54 +0300

    fix authentication in HomePage

--- frontend-client/src/pages/home/home.jsx
+++ frontend-client/src/pages/home/home.jsx
@@ -1,3 +1,11 @@
+import { Navigate } from "react-router-dom";
+import { Path } from "../../app/config/path";
+import { useAppSelector } from "../../shared/hooks/hooks";
+
 export const Home = () => {
+  const isLogined = useAppSelector((state) => state.login.isLogined);
+  if (!isLogined) {
+    return <Navigate to={Path.login} />;
+  }
   return <div>I home</div>;
 };
