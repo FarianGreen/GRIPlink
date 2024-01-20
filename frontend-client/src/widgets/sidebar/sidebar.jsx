7a369754ded3d21817e177a5379b2040dc09610f
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-20 14:48:30 +0300

    feature/sidebar.jsx: "change className for Link"

--- frontend-client/src/widgets/sidebar/sidebar.jsx
+++ frontend-client/src/widgets/sidebar/sidebar.jsx
@@ -1,20 +1,20 @@
-import { Path } from "../../app/config/path";
-import "./sidebar.scss";
-import { Link } from "react-router-dom";
+import { Path } from '../../app/config/path';
+import './sidebar.scss';
+import { Link } from 'react-router-dom';
 
 export const Sidebar = () => {
   return (
     <ul className="navbar">
-      <Link className="navbar__ul-item" to={Path.home}>
+      <Link className="navbar__item" to={Path.home}>
         Главная
       </Link>
-      <Link className="navbar__ul-item" to={Path.friends}>
+      <Link className="navbar__item" to={Path.friends}>
         Друзья
       </Link>
-      <Link className="navbar__ul-item" to={Path.messages}>
+      <Link className="navbar__item" to={Path.messages}>
         Сообщения
       </Link>
-      <Link className="navbar__ul-item" to={Path.clubs}>
+      <Link className="navbar__item" to={Path.clubs}>
         Клубы
       </Link>
     </ul>
