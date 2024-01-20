b277749a99ba6cd35d08cf46f09262325f20c19b
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-20 20:13:09 +0300

    feature/header: "add htmlin file"

--- frontend-client/src/widgets/header/header.tsx
+++ frontend-client/src/widgets/header/header.tsx
@@ -1,4 +1,53 @@
-import "./header.scss";
+import { Path } from '../../app/config/path';
+import { Link } from 'react-router-dom';
+
+import './header.scss';
 export const Header = () => {
-  return <div className="header-conatiner"> I header</div>;
+  return (
+    <section className="header">
+      <nav className="header__nav">
+        <div className="header__nav-burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className="header__nav-menu">
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Профиль
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Мессенджер
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Друзья
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Сообщество
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Расписание
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            События
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Фото
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Видео
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Музыка
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Файлы
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}>
+            Общие настройки
+          </Link>
+          <Link className="header__nav-menu-item" to={Path.home}></Link>
+        </ul>
+      </nav>
+    </section>
+  );
 };
