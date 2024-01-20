629c46aa1fd6b5389620514025523a7192c2b31a
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-20 16:28:25 +0300

    feature/sidebar: "import icons in sidebar.jsx"

--- frontend-client/src/widgets/sidebar/sidebar.jsx
+++ frontend-client/src/widgets/sidebar/sidebar.jsx
@@ -1,24 +1,29 @@
 import { Path } from '../../app/config/path';
 import './sidebar.scss';
 import { Link } from 'react-router-dom';
+import iconHouse from '../../assets/icons/sidebar-icons/house-icon.svg';
+import iconCalendar from '../../assets/icons/sidebar-icons/calendar-icon.svg';
+import iconMessage from '../../assets/icons/sidebar-icons/message-icon.svg';
+import iconJoystick from '../../assets/icons/sidebar-icons/joystick-icon.svg';
+import iconSearch from '../../assets/icons/sidebar-icons/search-icon.svg';
 
 export const Sidebar = () => {
   return (
     <ul className="navbar">
       <Link className="navbar__item" to={Path.home}>
-        {/* Главная */}
+        <img src={iconHouse} alt="" />
       </Link>
       <Link className="navbar__item" to={Path.friends}>
-        {/* Друзья */}
+        <img src={iconCalendar} alt="" />
       </Link>
       <Link className="navbar__item" to={Path.messages}>
-        {/* Сообщения */}
+        <img src={iconMessage} alt="" />
       </Link>
       <Link className="navbar__item" to={Path.clubs}>
-        {/* Клубы */}
+        <img src={iconJoystick} alt="" />
       </Link>
       <Link className="navbar__item" to={Path.search}>
-        {/* Клубы */}
+        <img src={iconSearch} alt="" />
       </Link>
     </ul>
   );
