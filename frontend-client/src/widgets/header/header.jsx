8a530b8394744a9f81e86787108750891ed6a271
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-20 22:18:13 +0300

    feature/header: "work on styles in file"

--- frontend-client/src/widgets/header/header.jsx
+++ frontend-client/src/widgets/header/header.jsx
@@ -1,7 +1,7 @@
 const menu = document.querySelector('.header__nav-menu');
 const menuBurger = document.querySelector('.header__nav-burger');
 
-menuBurger.addEventListener('touchstart', () => {
+menuBurger.addEventListener('click', () => {
   menu.classList.toggle('header__nav-menu--active');
   menuBurger.classList.toggle('header__nav-burger--active');
 });
