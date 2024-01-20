3b3354230ad9c21e7347db07d3fb24d9cec06311
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-20 13:56:19 +0300

    feature/sidebar.scss: "add styles for .sidebar"

--- frontend-client/src/app/config/layout.tsx
+++ frontend-client/src/app/config/layout.tsx
@@ -1,6 +1,6 @@
-import { Outlet } from "react-router-dom";
-import { Header } from "../../widgets/header/header";
-import { Sidebar } from "../../widgets/sidebar/sidebar";
+import { Outlet } from 'react-router-dom';
+import { Header } from '../../widgets/header/header';
+import { Sidebar } from '../../widgets/sidebar/sidebar';
 
 export const Layout = () => {
   return (
