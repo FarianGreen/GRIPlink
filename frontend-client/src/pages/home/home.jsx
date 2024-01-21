aa0cb71a7f8b60f9e02910698728db040fd171b9
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-21 11:59:35 +0300

    feature/home: "add section in file"

--- frontend-client/src/pages/home/home.jsx
+++ frontend-client/src/pages/home/home.jsx
@@ -1,11 +1,23 @@
-import { Navigate } from "react-router-dom";
-import { Path } from "../../app/config/path";
-import { useAppSelector } from "../../shared/hooks/hooks";
+import { Navigate } from 'react-router-dom';
+import { Path } from '../../app/config/path';
+import { useAppSelector } from '../../shared/hooks/hooks';
+
+import homeImg from '../../assets/img/home-img/home-img.jpg';
 
 export const Home = () => {
   const isLogined = useAppSelector((state) => state.login.isLogined);
   if (!isLogined) {
     return <Navigate to={Path.login} />;
   }
-  return <div>I home</div>;
+  return (
+    <section className="home">
+      <img src={homeImg} alt="" />
+      <div className="home__content">
+        <div className="home__content-nav"></div>
+        <div className="home__content-user"></div>
+        <div className="home__content-info"></div>
+        <div className="home__content-publication"></div>
+      </div>
+    </section>
+  );
 };
