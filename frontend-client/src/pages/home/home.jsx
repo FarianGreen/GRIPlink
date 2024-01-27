56858e80c115b54e13487a0816c05b96f61a1464
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-27 18:21:00 +0300

    feature/home: "working on home.jsx"

--- frontend-client/src/pages/home/home.jsx
+++ frontend-client/src/pages/home/home.jsx
@@ -1,5 +1,15 @@
 import './home.scss';
 
 export const Home = () => {
-  return <section className="home"></section>;
+  return (
+    <section className="home">
+      <div className="home__content">
+        <article className="home__content-card">
+          <div className="home__content-card-front"></div>
+          <div className="home__content-card-back"></div>
+        </article>
+        <button className="home__content-button"></button>
+      </div>
+    </section>
+  );
 };
