817455b03cc006c0d553e3148576db347ea23f1c
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-01-27 18:10:56 +0300

    feature/header: "work on styles"

--- frontend-client/src/pages/home/home.jsx
+++ frontend-client/src/pages/home/home.jsx
@@ -1,15 +1,5 @@
-import "./home.scss";
+import './home.scss';
 
 export const Home = () => {
-  return (
-    <section className="home">
-      <div className="home__photo"></div>
-      <div className="home__content">
-        <div className="home__content-nav"></div>
-        <div className="home__content-user"></div>
-        <div className="home__content-info"></div>
-        <div className="home__content-publication"></div>
-      </div>
-    </section>
-  );
+  return <section className="home"></section>;
 };
