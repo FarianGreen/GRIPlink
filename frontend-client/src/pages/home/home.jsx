0060b8fc46dbd3869d06898bf6d0c002e382eb02
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2024-02-04 18:01:21 +0300

    feature/home: "work on card"

--- frontend-client/src/pages/home/home.jsx
+++ frontend-client/src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import './home.scss';
 
 import homeImg from '../../assets/img/home-img/home-img.jpg';
 import mainBackground from '../../assets/img/main-background/background.jpg';
+
 import { useAppDispatch, useAppSelector } from '../../shared/hooks/hooks';
 import { toggleCard } from './reducer/cardSlice';
 
@@ -32,12 +33,104 @@ export const Home = () => {
               </span>
             </div>
           </div>
+
           <div className="home__content-card-back">
+            <button className="home__content-card-back-exit"></button>
             <img
               src={mainBackground}
               alt=""
               className="home__content-card-back-picture"
             />
+
+            <form
+              action="#"
+              method="get"
+              className="home__content-card-back-profile"
+            >
+              <div className="home__content-card-back-profile-item">
+                <h6 className="home__content-card-back-profile-item-title">
+                  Имя:
+                </h6>
+                <input
+                  className="home__content-card-back-profile-item-info"
+                  name="username"
+                  type="text"
+                />
+              </div>
+
+              <div className="home__content-card-back-profile-item">
+                <h6 className="home__content-card-back-profile-item-title">
+                  Фамилия:
+                </h6>
+                <input
+                  className="home__content-card-back-profile-item-info"
+                  name="surname"
+                  type="text"
+                />
+              </div>
+
+              <div className="home__content-card-back-profile-item">
+                <h6 className="home__content-card-back-profile-item-title">
+                  Пол:
+                </h6>
+                <input
+                  className="home__content-card-back-profile-item-info"
+                  name="gender"
+                  type="text"
+                />
+              </div>
+
+              <div className="home__content-card-back-profile-item">
+                <h6 className="home__content-card-back-profile-item-title">
+                  Дата рождения:
+                </h6>
+                <input
+                  className="home__content-card-back-profile-item-info"
+                  name="date-of-birth"
+                  type="text"
+                />
+              </div>
+
+              <div className="home__content-card-back-profile-item">
+                <h6 className="home__content-card-back-profile-item-title">
+                  Категория:
+                </h6>
+                <input
+                  className="home__content-card-back-profile-item-info"
+                  name="category"
+                  type="text"
+                />
+              </div>
+
+              <div className="home__content-card-back-profile-item">
+                <h6 className="home__content-card-back-profile-item-title">
+                  Вес:
+                </h6>
+                <input
+                  className="home__content-card-back-profile-item-info"
+                  name="weight"
+                  type="text"
+                />
+              </div>
+
+              <div className="home__content-card-back-profile-item">
+                <h6 className="home__content-card-back-profile-item-title">
+                  Пояс:
+                </h6>
+                <input
+                  className="home__content-card-back-profile-item-info"
+                  name="belt"
+                  type="text"
+                />
+              </div>
+
+              <button
+                className="home__content-card-back-profile-button"
+                type="submit"
+              >
+                Сохранить
+              </button>
+            </form>
           </div>
         </article>
         <button className="home__content-button"></button>
